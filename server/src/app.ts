c65eb6da312cdcb4c4925a97a9c51763de9ee35d
zeeshan-kodex <ahsansahi6@gmail.com>
2026-09-01 11:14:36 +0500

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,8 @@ import morgan from "morgan";
 import errorHandler from "./middleware/error.handler.js";
 import categoryRoutes from "./routes/category.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
@@ -12,6 +14,8 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/product", productRoutes);
 app.use(errorHandler);
